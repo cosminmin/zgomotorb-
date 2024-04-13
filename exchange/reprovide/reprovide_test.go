26f77bac18f972799b7fc89f672037e18b9a11b3
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:32:20 +0200

    Initialize feat/provider-strategies-1

---
+++ exchange/reprovide/reprovide_test.go
@@ -0,0 +1,57 @@
+package reprovide_test
+
+import (
+	"context"
+	"testing"
+
+	blockstore "github.com/ipfs/go-ipfs/blocks/blockstore"
+	mock "github.com/ipfs/go-ipfs/routing/mock"
+	pstore "gx/ipfs/QmPgDWmTmuzvP7QE5zwo1TmjbJme9pmZHNujB2453jkCTr/go-libp2p-peerstore"
+	testutil "gx/ipfs/QmQgLZP9haZheimMHqqAjJh2LhRmNfEoZDfbtkpeMhi9xK/go-testutil"
+	blocks "gx/ipfs/QmSn9Td7xgxm9EV7iEjTckpUWmWApggzPxu7eFGWkkpwin/go-block-format"
+	ds "gx/ipfs/QmdHG8MAuARdGHxx4rPQASLcvhz24fzjSQq7AJRAQEorq5/go-datastore"
+	dssync "gx/ipfs/QmdHG8MAuARdGHxx4rPQASLcvhz24fzjSQq7AJRAQEorq5/go-datastore/sync"
+
+	. "github.com/ipfs/go-ipfs/exchange/reprovide"
+)
+
+func TestReprovide(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	mrserv := mock.NewServer()
+
+	idA := testutil.RandIdentityOrFatal(t)
+	idB := testutil.RandIdentityOrFatal(t)
+
+	clA := mrserv.Client(idA)
+	clB := mrserv.Client(idB)
+
+	bstore := blockstore.NewBlockstore(dssync.MutexWrap(ds.NewMapDatastore()))
+
+	blk := blocks.NewBlock([]byte("this is a test"))
+	bstore.Put(blk)
+
+	keyProvider := NewBlockstoreProvider(bstore)
+	reprov := NewReprovider(ctx, clA, keyProvider)
+	err := reprov.Reprovide()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var providers []pstore.PeerInfo
+	maxProvs := 100
+
+	provChan := clB.FindProvidersAsync(ctx, blk.Cid(), maxProvs)
+	for p := range provChan {
+		providers = append(providers, p)
+	}
+
+	if len(providers) == 0 {
+		t.Fatal("Should have gotten a provider")
+	}
+
+	if providers[0].ID != idA.ID() {
+		t.Fatal("Somehow got the wrong peer back as a provider.")
+	}
+}
---
+++ importer/importer.go
@@ -0,0 +1,59 @@
+// package importer implements utilities used to create IPFS DAGs from files
+// and readers
+package importer
+
+import (
+	"fmt"
+	"os"
+
+	"gx/ipfs/QmUyfy4QSr3NXym4etEiRyxBLqqAeKHJuRdi8AACxg63fZ/go-ipfs-cmdkit/files"
+
+	bal "github.com/ipfs/go-ipfs/importer/balanced"
+	"github.com/ipfs/go-ipfs/importer/chunk"
+	h "github.com/ipfs/go-ipfs/importer/helpers"
+	trickle "github.com/ipfs/go-ipfs/importer/trickle"
+	dag "github.com/ipfs/go-ipfs/merkledag"
+
+	node "gx/ipfs/QmPN7cwmpcc4DWXb4KTB9dNAJgjuPY69h3npsMfhRrQL9c/go-ipld-format"
+)
+
+// Builds a DAG from the given file, writing created blocks to disk as they are
+// created
+func BuildDagFromFile(fpath string, ds dag.DAGService) (node.Node, error) {
+	stat, err := os.Lstat(fpath)
+	if err != nil {
+		return nil, err
+	}
+
+	if stat.IsDir() {
+		return nil, fmt.Errorf("`%s` is a directory", fpath)
+	}
+
+	f, err := files.NewSerialFile(fpath, fpath, false, stat)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return BuildDagFromReader(ds, chunk.NewSizeSplitter(f, chunk.DefaultBlockSize))
+}
+
+// BuildDagFromReader creates new DAG containing data provided by Splitter
+func BuildDagFromReader(ds dag.DAGService, spl chunk.Splitter) (node.Node, error) {
+	dbp := h.DagBuilderParams{
+		Dagserv:  ds,
+		Maxlinks: h.DefaultLinksPerBlock,
+	}
+
+	return bal.BalancedLayout(dbp.New(spl))
+}
+
+// BuildTrickleDagFromReader creates new DAG with trickle layout containing data provided by Splitter
+func BuildTrickleDagFromReader(ds dag.DAGService, spl chunk.Splitter) (node.Node, error) {
+	dbp := h.DagBuilderParams{
+		Dagserv:  ds,
+		Maxlinks: h.DefaultLinksPerBlock,
+	}
+
+	return trickle.TrickleLayout(dbp.New(spl))
+}
