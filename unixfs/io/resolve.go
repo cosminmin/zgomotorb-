69c6e8595df8855df1b544c6c14fec5d9ba64f16
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:33:35 +0200

    Initialize fix/5309

---
+++ unixfs/io/resolve.go
@@ -0,0 +1,58 @@
+package io
+
+import (
+	"context"
+
+	ft "github.com/ipfs/go-ipfs/unixfs"
+	hamt "github.com/ipfs/go-ipfs/unixfs/hamt"
+	dag "gx/ipfs/QmRy4Qk9hbgFX9NGJRm8rBThrA8PZhNCitMgeRYyZ67s59/go-merkledag"
+
+	ipld "gx/ipfs/QmZtNq8dArGfnpCZfx2pUNY7UcjGhVp5qqwQ4hH6mpTMRQ/go-ipld-format"
+)
+
+// ResolveUnixfsOnce resolves a single hop of a path through a graph in a
+// unixfs context. This includes handling traversing sharded directories.
+func ResolveUnixfsOnce(ctx context.Context, ds ipld.NodeGetter, nd ipld.Node, names []string) (*ipld.Link, []string, error) {
+	switch nd := nd.(type) {
+	case *dag.ProtoNode:
+		upb, err := ft.FromBytes(nd.Data())
+		if err != nil {
+			// Not a unixfs node, use standard object traversal code
+			lnk, err := nd.GetNodeLink(names[0])
+			if err != nil {
+				return nil, nil, err
+			}
+
+			return lnk, names[1:], nil
+		}
+
+		switch upb.GetType() {
+		case ft.THAMTShard:
+			rods := dag.NewReadOnlyDagService(ds)
+			s, err := hamt.NewHamtFromDag(rods, nd)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			out, err := s.Find(ctx, names[0])
+			if err != nil {
+				return nil, nil, err
+			}
+
+			return out, names[1:], nil
+		default:
+			lnk, err := nd.GetNodeLink(names[0])
+			if err != nil {
+				return nil, nil, err
+			}
+
+			return lnk, names[1:], nil
+		}
+	default:
+		lnk, rest, err := nd.ResolveLink(names)
+		if err != nil {
+			return nil, nil, err
+		}
+		return lnk, rest, nil
+	}
+}
