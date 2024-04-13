3c18e2893157888f75af405ca0cbb368fb5e355b
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:41:19 +0200

    Initialize petar/nodiv

---
+++ assets/bindata_version_hash.go
@@ -0,0 +1,6 @@
+// File generated together with 'bindata.go' when running `go generate .` DO NOT EDIT. (@generated)
+package assets
+
+const (
+	BindataVersionHash = "60ae1cfa3f31134017737125db89f905dd3eea98"
+)
