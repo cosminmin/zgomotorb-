e5446897a8a25220d7ab79b576c1d262e8256255
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:41:29 +0200

    Initialize chore/ci-deps

---
+++ assets/bindata_version_hash.go
@@ -0,0 +1,6 @@
+// File generated together with 'bindata.go' when running `go generate .` DO NOT EDIT. (@generated)
+package assets
+
+const (
+	BindataVersionHash = "514e5ae28d8adb84955801b56ef47aca44bf9cc8"
+)
