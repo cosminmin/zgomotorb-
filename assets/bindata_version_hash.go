b61b066e766e609c07d2df98e65f5d468a4607ec
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:41:13 +0200

    Initialize fix/go-bindata

---
+++ assets/bindata_version_hash.go
@@ -0,0 +1,6 @@
+// File generated together with 'bindata.go' when running `go generate .` DO NOT EDIT. (@generated)
+package assets
+
+const (
+	BindataVersionHash = "f8497893ebd3e47fe8ff172d4b60b4315a0006ee"
+)
