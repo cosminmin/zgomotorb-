bc7f0a42cad362db1c7943344e61287ec3b5a914
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:34:48 +0200

    Initialize feat/go-get-meta-tag

---
+++ core/commands/dht_test.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/ipfs/go-ipfs/namesys"
+
+	ipns "gx/ipfs/QmPrt2JqvtFcgMBmYBjtZ5jFzq6HoFXy8PTwLb2Dpm2cGf/go-ipns"
+	tu "gx/ipfs/QmPuhRE325DR8ChNcFtgd6F1eANCHy1oohXZPpYop4xsK6/go-testutil"
+)
+
+func TestKeyTranslation(t *testing.T) {
+	pid := tu.RandPeerIDFatal(t)
+	pkname := namesys.PkKeyForID(pid)
+	ipnsname := ipns.RecordKey(pid)
+
+	pkk, err := escapeDhtKey("/pk/" + pid.Pretty())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ipnsk, err := escapeDhtKey("/ipns/" + pid.Pretty())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if pkk != pkname {
+		t.Fatal("keys didnt match!")
+	}
+
+	if ipnsk != ipnsname {
+		t.Fatal("keys didnt match!")
+	}
+}
