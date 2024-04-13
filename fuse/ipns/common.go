2403f28de3460ad0dda376fa819acd420552db21
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:35:01 +0200

    Initialize fix/mfs-flush

---
+++ fuse/ipns/common.go
@@ -0,0 +1,34 @@
+package ipns
+
+import (
+	"context"
+
+	"github.com/ipfs/go-ipfs/core"
+	nsys "github.com/ipfs/go-ipfs/namesys"
+	path "gx/ipfs/QmNYPETsdAu2uQ1k9q9S1jYEGURaLHV6cbYRSVFVRftpF8/go-path"
+	ci "gx/ipfs/QmNiJiXwWE3kRhZrC5ej3kSjWHm337pYfhjLGSCDNKJP2s/go-libp2p-crypto"
+	ft "gx/ipfs/QmQXze9tG878pa4Euya4rrDpyTNX3kQe4dhCaBzBozGgpe/go-unixfs"
+)
+
+// InitializeKeyspace sets the ipns record for the given key to
+// point to an empty directory.
+func InitializeKeyspace(n *core.IpfsNode, key ci.PrivKey) error {
+	ctx, cancel := context.WithCancel(n.Context())
+	defer cancel()
+
+	emptyDir := ft.EmptyDirNode()
+
+	err := n.Pinning.Pin(ctx, emptyDir, false)
+	if err != nil {
+		return err
+	}
+
+	err = n.Pinning.Flush()
+	if err != nil {
+		return err
+	}
+
+	pub := nsys.NewIpnsPublisher(n.Routing, n.Repo.Datastore())
+
+	return pub.Publish(ctx, key, path.FromCid(emptyDir.Cid()))
+}
