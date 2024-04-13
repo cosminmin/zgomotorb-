8e41091bbd0096ffbd0ea534f5d3f8b33a974828
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:34:39 +0200

    Initialize feat/always-load-routing

---
+++ core/mock/mock.go
@@ -0,0 +1,74 @@
+package coremock
+
+import (
+	"context"
+
+	commands "github.com/ipfs/go-ipfs/commands"
+	core "github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/repo"
+
+	testutil "gx/ipfs/QmPuhRE325DR8ChNcFtgd6F1eANCHy1oohXZPpYop4xsK6/go-testutil"
+	libp2p "gx/ipfs/QmRBaUEQEeFWywfrZJ64QgsmvcqgLSK3VbvGMR2NM2Edpf/go-libp2p"
+	mocknet "gx/ipfs/QmRBaUEQEeFWywfrZJ64QgsmvcqgLSK3VbvGMR2NM2Edpf/go-libp2p/p2p/net/mock"
+	peer "gx/ipfs/QmY5Grm8pJdiSSVsYxx4uNRgweY72EmYwuSDbRnbFok3iY/go-libp2p-peer"
+	config "gx/ipfs/QmYyzmMnhNTtoXx5ttgUaRdHHckYnQWjPL98hgLAR2QLDD/go-ipfs-config"
+	pstore "gx/ipfs/QmZ9zH2FnLcxv1xyzFeUpDUeo55xEhZQHgveZijcxr7TLj/go-libp2p-peerstore"
+	datastore "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore"
+	syncds "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore/sync"
+	host "gx/ipfs/QmfD51tKgJiTMnW9JEiDiPwsCY4mqUoxkhKhBfyW12spTC/go-libp2p-host"
+)
+
+// NewMockNode constructs an IpfsNode for use in tests.
+func NewMockNode() (*core.IpfsNode, error) {
+	ctx := context.Background()
+
+	// effectively offline, only peer in its network
+	return core.NewNode(ctx, &core.BuildCfg{
+		Online: true,
+		Host:   MockHostOption(mocknet.New(ctx)),
+	})
+}
+
+func MockHostOption(mn mocknet.Mocknet) core.HostOption {
+	return func(ctx context.Context, id peer.ID, ps pstore.Peerstore, _ ...libp2p.Option) (host.Host, error) {
+		return mn.AddPeerWithPeerstore(id, ps)
+	}
+}
+
+func MockCmdsCtx() (commands.Context, error) {
+	// Generate Identity
+	ident, err := testutil.RandIdentity()
+	if err != nil {
+		return commands.Context{}, err
+	}
+	p := ident.ID()
+
+	conf := config.Config{
+		Identity: config.Identity{
+			PeerID: p.String(),
+		},
+	}
+
+	r := &repo.Mock{
+		D: syncds.MutexWrap(datastore.NewMapDatastore()),
+		C: conf,
+	}
+
+	node, err := core.NewNode(context.Background(), &core.BuildCfg{
+		Repo: r,
+	})
+	if err != nil {
+		return commands.Context{}, err
+	}
+
+	return commands.Context{
+		Online:     true,
+		ConfigRoot: "/tmp/.mockipfsconfig",
+		LoadConfig: func(path string) (*config.Config, error) {
+			return &conf, nil
+		},
+		ConstructNode: func() (*core.IpfsNode, error) {
+			return node, nil
+		},
+	}, nil
+}
