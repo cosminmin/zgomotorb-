ff2243631c14cef1a68bd17805000ccc1a8b1949
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:33:28 +0200

    Initialize kevina/strict-cids-wip

---
+++ core/coredag/raw.go
@@ -0,0 +1,37 @@
+package coredag
+
+import (
+	"io"
+	"io/ioutil"
+	"math"
+
+	"github.com/ipfs/go-ipfs/merkledag"
+
+	mh "gx/ipfs/QmPnFwZ2JXKnXgMw8CdBPxn7FWh6LLdjUjxV1fKHuJnkr8/go-multihash"
+	block "gx/ipfs/QmTRCUvZLiir12Qr6MV3HKfKMHX8Nf1Vddn6t2g5nsQSb9/go-block-format"
+	ipld "gx/ipfs/QmWi2BYBL5gJ3CiAiQchg6rn1A8iBsrWy51EYxvHVjFvLb/go-ipld-format"
+	cid "gx/ipfs/QmapdYm1b22Frv3k17fqrBYTFRxwiaVJkB299Mfn33edeB/go-cid"
+)
+
+func rawRawParser(r io.Reader, mhType uint64, mhLen int) ([]ipld.Node, error) {
+	if mhType == math.MaxUint64 {
+		mhType = mh.SHA2_256
+	}
+
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	prefix := cid.NewPrefixV1(cid.Raw, mhType, mhLen)
+	c, err := prefix.Sum(data)
+	if err != nil {
+		return nil, err
+	}
+	blk, err := block.NewBlockWithCid(data, c)
+	if err != nil {
+		return nil, err
+	}
+	nd := &merkledag.RawNode{Block: blk}
+	return []ipld.Node{nd}, nil
+}
---
+++ namesys/publisher_test.go
@@ -0,0 +1,112 @@
+package namesys
+
+import (
+	"context"
+	"crypto/rand"
+	"testing"
+	"time"
+
+	dshelp "gx/ipfs/QmNP2u7bofwUQptHQGPfabGWtTCbxhNLSZKqbf1uzsup9V/go-ipfs-ds-help"
+	testutil "gx/ipfs/QmPdxCaVp4jZ9RbxqZADvKH6kiCR5jHvdR5f2ycjAY6T2a/go-testutil"
+	ipns "gx/ipfs/QmRAPFFaF7nrezCZQaLihyp2qbAXqSJU5WpvSpwroMv1Xt/go-ipns"
+	ma "gx/ipfs/QmUxSEGbv2nmYNnfXi7839wwQqTN3kwQeUxe8dTjZWZs7J/go-multiaddr"
+	peer "gx/ipfs/QmVf8hTAsLLFtn4WPCRNdnaF2Eag2qTBS6uR8AiHPZARXy/go-libp2p-peer"
+	mockrouting "gx/ipfs/Qmb1N7zdjG2FexpzWNj8T289u9QnQLEiSsTRadDGQxX32D/go-ipfs-routing/mock"
+	ci "gx/ipfs/Qme1knMqwt1hKZbc1BmQFmnm9f36nyQGwXxPGVpVJ9rMK5/go-libp2p-crypto"
+	ds "gx/ipfs/QmeiCcJfDW1GJnWUArudsv5rQsihpi4oyddPhdqo3CfX6i/go-datastore"
+	dssync "gx/ipfs/QmeiCcJfDW1GJnWUArudsv5rQsihpi4oyddPhdqo3CfX6i/go-datastore/sync"
+)
+
+type identity struct {
+	testutil.PeerNetParams
+}
+
+func (p *identity) ID() peer.ID {
+	return p.PeerNetParams.ID
+}
+
+func (p *identity) Address() ma.Multiaddr {
+	return p.Addr
+}
+
+func (p *identity) PrivateKey() ci.PrivKey {
+	return p.PrivKey
+}
+
+func (p *identity) PublicKey() ci.PubKey {
+	return p.PubKey
+}
+
+func testNamekeyPublisher(t *testing.T, keyType int, expectedErr error, expectedExistence bool) {
+	// Context
+	ctx := context.Background()
+
+	// Private key
+	privKey, pubKey, err := ci.GenerateKeyPairWithReader(keyType, 2048, rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ID
+	id, err := peer.IDFromPublicKey(pubKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Value
+	value := []byte("ipfs/TESTING")
+
+	// Seqnum
+	seqnum := uint64(0)
+
+	// Eol
+	eol := time.Now().Add(24 * time.Hour)
+
+	// Routing value store
+	p := testutil.PeerNetParams{
+		ID:      id,
+		PrivKey: privKey,
+		PubKey:  pubKey,
+		Addr:    testutil.ZeroLocalTCPAddress,
+	}
+
+	dstore := dssync.MutexWrap(ds.NewMapDatastore())
+	serv := mockrouting.NewServer()
+	r := serv.ClientWithDatastore(context.Background(), &identity{p}, dstore)
+
+	entry, err := ipns.Create(privKey, value, seqnum, eol)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = PutRecordToRouting(ctx, r, pubKey, entry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check for namekey existence in value store
+	namekey := PkKeyForID(id)
+	_, err = r.GetValue(ctx, namekey)
+	if err != expectedErr {
+		t.Fatal(err)
+	}
+
+	// Also check datastore for completeness
+	key := dshelp.NewKeyFromBinary([]byte(namekey))
+	exists, err := dstore.Has(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if exists != expectedExistence {
+		t.Fatal("Unexpected key existence in datastore")
+	}
+}
+
+func TestRSAPublisher(t *testing.T) {
+	testNamekeyPublisher(t, ci.RSA, nil, true)
+}
+
+func TestEd22519Publisher(t *testing.T) {
+	testNamekeyPublisher(t, ci.Ed25519, ds.ErrNotFound, false)
+}
