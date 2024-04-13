b90f9d294d55d15d97365acf6197c849b446462a
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:33:15 +0200

    Initialize kevina/strict-cids

---
+++ core/commands/dht_test.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/ipfs/go-ipfs/namesys"
+
+	tu "gx/ipfs/QmPdxCaVp4jZ9RbxqZADvKH6kiCR5jHvdR5f2ycjAY6T2a/go-testutil"
+	ipns "gx/ipfs/QmRAPFFaF7nrezCZQaLihyp2qbAXqSJU5WpvSpwroMv1Xt/go-ipns"
+)
+
+func TestKeyTranslation(t *testing.T) {
+	pid := tu.RandPeerIDFatal(t)
+	pkname := namesys.PkKeyForID(pid)
+	ipnsname := ipns.RecordKey(pid)
+
+	pkk, err := escapeDhtKey("/pk/" + pid.Pretty())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ipnsk, err := escapeDhtKey("/ipns/" + pid.Pretty())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if pkk != pkname {
+		t.Fatal("keys didnt match!")
+	}
+
+	if ipnsk != ipnsname {
+		t.Fatal("keys didnt match!")
+	}
+}
---
+++ exchange/bitswap/decision/bench_test.go
@@ -0,0 +1,30 @@
+package decision
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/ipfs/go-ipfs/exchange/bitswap/wantlist"
+	u "gx/ipfs/QmPdKqUcHGFdeSpvjVoaTRPPstGif9GBZb5Q56RVw9o69A/go-ipfs-util"
+	"gx/ipfs/QmPdxCaVp4jZ9RbxqZADvKH6kiCR5jHvdR5f2ycjAY6T2a/go-testutil"
+	"gx/ipfs/QmVf8hTAsLLFtn4WPCRNdnaF2Eag2qTBS6uR8AiHPZARXy/go-libp2p-peer"
+	cid "gx/ipfs/QmapdYm1b22Frv3k17fqrBYTFRxwiaVJkB299Mfn33edeB/go-cid"
+)
+
+// FWIW: At the time of this commit, including a timestamp in task increases
+// time cost of Push by 3%.
+func BenchmarkTaskQueuePush(b *testing.B) {
+	q := newPRQ()
+	peers := []peer.ID{
+		testutil.RandPeerIDFatal(b),
+		testutil.RandPeerIDFatal(b),
+		testutil.RandPeerIDFatal(b),
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c := cid.NewCidV0(u.Hash([]byte(fmt.Sprint(i))))
+
+		q.Push(&wantlist.Entry{Cid: c, Priority: math.MaxInt32}, peers[i%len(peers)])
+	}
+}
