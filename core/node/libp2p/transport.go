6cc8c26b10efb3cca7871ef90aaa020f1c08e21c
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:39:57 +0200

    Initialize feat/long-cids-in-subdomain-gw

---
+++ core/node/libp2p/transport.go
@@ -0,0 +1,44 @@
+package libp2p
+
+import (
+	"github.com/libp2p/go-libp2p"
+	metrics "github.com/libp2p/go-libp2p-core/metrics"
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
+	secio "github.com/libp2p/go-libp2p-secio"
+	tls "github.com/libp2p/go-libp2p-tls"
+
+	"go.uber.org/fx"
+)
+
+func Transports(pnet struct {
+	fx.In
+	Fprint PNetFingerprint `optional:"true"`
+}) (opts Libp2pOpts) {
+	opts.Opts = append(opts.Opts, libp2p.DefaultTransports)
+	if pnet.Fprint == nil {
+		opts.Opts = append(opts.Opts, libp2p.Transport(libp2pquic.NewTransport))
+	}
+	return opts
+}
+
+func Security(enabled bool) interface{} {
+	if !enabled {
+		return func() (opts Libp2pOpts) {
+			// TODO: shouldn't this be Errorf to guarantee visibility?
+			log.Warnf(`Your IPFS node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
+		You will not be able to connect to any nodes configured to use encrypted connections`)
+			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
+			return opts
+		}
+	}
+	return func() (opts Libp2pOpts) {
+		opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(secio.ID, secio.New)))
+		return opts
+	}
+}
+
+func BandwidthCounter() (opts Libp2pOpts, reporter *metrics.BandwidthCounter) {
+	reporter = metrics.NewBandwidthCounter()
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
+	return opts, reporter
+}
