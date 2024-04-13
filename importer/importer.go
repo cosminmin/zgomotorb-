77d417022411428a8262ee34b05b90980407997e
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:32:23 +0200

    Initialize feat/get-tar-stdout

---
+++ importer/importer.go
@@ -0,0 +1,56 @@
+// package importer implements utilities used to create IPFS DAGs from files
+// and readers
+package importer
+
+import (
+	"fmt"
+	"os"
+
+	bal "github.com/ipfs/go-ipfs/importer/balanced"
+	"github.com/ipfs/go-ipfs/importer/chunk"
+	h "github.com/ipfs/go-ipfs/importer/helpers"
+	trickle "github.com/ipfs/go-ipfs/importer/trickle"
+	dag "github.com/ipfs/go-ipfs/merkledag"
+	"gx/ipfs/QmQp2a2Hhb7F6eK2A5hN8f9aJy4mtkEikL9Zj4cgB7d1dD/go-ipfs-cmdkit/files"
+
+	node "gx/ipfs/QmNwUEK7QbwSqyKBu3mMtToo8SUc6wQJ7gdZq4gGGJqfnf/go-ipld-format"
+)
+
+// Builds a DAG from the given file, writing created blocks to disk as they are
+// created
+func BuildDagFromFile(fpath string, ds dag.DAGService) (node.Node, error) {
+	stat, err := os.Lstat(fpath)
+	if err != nil {
+		return nil, err
+	}
+
+	if stat.IsDir() {
+		return nil, fmt.Errorf("`%s` is a directory", fpath)
+	}
+
+	f, err := files.NewSerialFile(fpath, fpath, false, stat)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return BuildDagFromReader(ds, chunk.NewSizeSplitter(f, chunk.DefaultBlockSize))
+}
+
+func BuildDagFromReader(ds dag.DAGService, spl chunk.Splitter) (node.Node, error) {
+	dbp := h.DagBuilderParams{
+		Dagserv:  ds,
+		Maxlinks: h.DefaultLinksPerBlock,
+	}
+
+	return bal.BalancedLayout(dbp.New(spl))
+}
+
+func BuildTrickleDagFromReader(ds dag.DAGService, spl chunk.Splitter) (node.Node, error) {
+	dbp := h.DagBuilderParams{
+		Dagserv:  ds,
+		Maxlinks: h.DefaultLinksPerBlock,
+	}
+
+	return trickle.TrickleLayout(dbp.New(spl))
+}
