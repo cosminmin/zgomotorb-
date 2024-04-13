b89f35b431cc0ea7f4a801dd2e4fb6328471d2e1
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:33:08 +0200

    Initialize fix/pin-stream

---
+++ exchange/reprovide/reprovide_test.go
@@ -0,0 +1,57 @@
+package reprovide_test
+
+import (
+	"context"
+	"testing"
+
+	testutil "gx/ipfs/QmPdxCaVp4jZ9RbxqZADvKH6kiCR5jHvdR5f2ycjAY6T2a/go-testutil"
+	blocks "gx/ipfs/QmTRCUvZLiir12Qr6MV3HKfKMHX8Nf1Vddn6t2g5nsQSb9/go-block-format"
+	pstore "gx/ipfs/QmZhsmorLpD9kmQ4ynbAu4vbKv2goMUnXazwGA4gnWHDjB/go-libp2p-peerstore"
+	mock "gx/ipfs/Qmb1N7zdjG2FexpzWNj8T289u9QnQLEiSsTRadDGQxX32D/go-ipfs-routing/mock"
+	blockstore "gx/ipfs/QmbaPGg81pvQiC5vTXtC9Jo8rdrWUjRaugH71WYNsgi6Ev/go-ipfs-blockstore"
+	ds "gx/ipfs/QmeiCcJfDW1GJnWUArudsv5rQsihpi4oyddPhdqo3CfX6i/go-datastore"
+	dssync "gx/ipfs/QmeiCcJfDW1GJnWUArudsv5rQsihpi4oyddPhdqo3CfX6i/go-datastore/sync"
+
+	. "github.com/ipfs/go-ipfs/exchange/reprovide"
+)
+
+func TestReprovide(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	mrserv := mock.NewServer()
+
+	idA := testutil.RandIdentityOrFatal(t)
+	idB := testutil.RandIdentityOrFatal(t)
+
+	clA := mrserv.Client(idA)
+	clB := mrserv.Client(idB)
+
+	bstore := blockstore.NewBlockstore(dssync.MutexWrap(ds.NewMapDatastore()))
+
+	blk := blocks.NewBlock([]byte("this is a test"))
+	bstore.Put(blk)
+
+	keyProvider := NewBlockstoreProvider(bstore)
+	reprov := NewReprovider(ctx, clA, keyProvider)
+	err := reprov.Reprovide()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var providers []pstore.PeerInfo
+	maxProvs := 100
+
+	provChan := clB.FindProvidersAsync(ctx, blk.Cid(), maxProvs)
+	for p := range provChan {
+		providers = append(providers, p)
+	}
+
+	if len(providers) == 0 {
+		t.Fatal("Should have gotten a provider")
+	}
+
+	if providers[0].ID != idA.ID() {
+		t.Fatal("Somehow got the wrong peer back as a provider.")
+	}
+}
---
+++ plugin/ipld.go
@@ -0,0 +1,16 @@
+package plugin
+
+import (
+	"github.com/ipfs/go-ipfs/core/coredag"
+
+	ipld "gx/ipfs/QmWi2BYBL5gJ3CiAiQchg6rn1A8iBsrWy51EYxvHVjFvLb/go-ipld-format"
+)
+
+// PluginIPLD is an interface that can be implemented to add handlers for
+// for different IPLD formats
+type PluginIPLD interface {
+	Plugin
+
+	RegisterBlockDecoders(dec ipld.BlockDecoder) error
+	RegisterInputEncParsers(iec coredag.InputEncParsers) error
+}
