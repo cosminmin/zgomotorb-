5506a60dca6a94428c5b8a6354ecd97d9e35ab47
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:34:35 +0200

    Initialize p2p/report-remote

---
+++ namesys/ipns_resolver_validation_test.go
@@ -0,0 +1,203 @@
+package namesys
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	path "gx/ipfs/QmQtg7N4XjAk2ZYpBjjv8B6gQprsRekabHBCnF6i46JYKJ/go-path"
+
+	opts "github.com/ipfs/go-ipfs/namesys/opts"
+
+	ci "gx/ipfs/QmNiJiXwWE3kRhZrC5ej3kSjWHm337pYfhjLGSCDNKJP2s/go-libp2p-crypto"
+	u "gx/ipfs/QmNohiVssaPw3KVLZik59DBVGTSm2dGvYT9eoXt5DQ36Yz/go-ipfs-util"
+	pstore "gx/ipfs/QmQAGG1zxfePqj2t7bLxyN8AFccZ889DDR9Gn8kVLDrGZo/go-libp2p-peerstore"
+	pstoremem "gx/ipfs/QmQAGG1zxfePqj2t7bLxyN8AFccZ889DDR9Gn8kVLDrGZo/go-libp2p-peerstore/pstoremem"
+	ipns "gx/ipfs/QmR9UpasSQR4Mqq1qiJAfnY4SVBxJn7r639CxiLjx8dYGm/go-ipns"
+	record "gx/ipfs/QmSoeYGNm8v8jAF49hX7UwHwkXjoeobSrn9sya5NPPsxXP/go-libp2p-record"
+	routing "gx/ipfs/QmZBH87CAPFHcc7cYmBqeSQ98zQ3SX9KUxiYgzPmLWNVKz/go-libp2p-routing"
+	ropts "gx/ipfs/QmZBH87CAPFHcc7cYmBqeSQ98zQ3SX9KUxiYgzPmLWNVKz/go-libp2p-routing/options"
+	testutil "gx/ipfs/QmZXjR5X1p4KrQ967cTsy4MymMzUM8mZECF3PV8UcN4o3g/go-testutil"
+	peer "gx/ipfs/QmcqU6QUDSXprb1518vYDGczrTJTyGwLG9eUa5iNX4xUtS/go-libp2p-peer"
+	mockrouting "gx/ipfs/QmdxhyAwBrnmJFsYPK6tyHh4Yy3gK8gbULErX1dRnpUMqu/go-ipfs-routing/mock"
+	offline "gx/ipfs/QmdxhyAwBrnmJFsYPK6tyHh4Yy3gK8gbULErX1dRnpUMqu/go-ipfs-routing/offline"
+	ds "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore"
+	dssync "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore/sync"
+)
+
+func TestResolverValidation(t *testing.T) {
+	ctx := context.Background()
+	rid := testutil.RandIdentityOrFatal(t)
+	dstore := dssync.MutexWrap(ds.NewMapDatastore())
+	peerstore := pstoremem.NewPeerstore()
+
+	vstore := newMockValueStore(rid, dstore, peerstore)
+	resolver := NewIpnsResolver(vstore)
+
+	nvVstore := offline.NewOfflineRouter(dstore, mockrouting.MockValidator{})
+
+	// Create entry with expiry in one hour
+	priv, id, _, ipnsDHTPath := genKeys(t)
+	ts := time.Now()
+	p := []byte("/ipfs/QmfM2r8seH2GiRaC4esTjeraXEachRt8ZsSeGaWTPLyMoG")
+	entry, err := ipns.Create(priv, p, 1, ts.Add(time.Hour))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Make peer's public key available in peer store
+	err = peerstore.AddPubKey(id, priv.GetPublic())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Publish entry
+	err = PublishEntry(ctx, vstore, ipnsDHTPath, entry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Resolve entry
+	resp, err := resolve(ctx, resolver, id.Pretty(), opts.DefaultResolveOpts())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp != path.Path(p) {
+		t.Fatalf("Mismatch between published path %s and resolved path %s", p, resp)
+	}
+	// Create expired entry
+	expiredEntry, err := ipns.Create(priv, p, 1, ts.Add(-1*time.Hour))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Publish entry
+	err = PublishEntry(ctx, nvVstore, ipnsDHTPath, expiredEntry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Record should fail validation because entry is expired
+	_, err = resolve(ctx, resolver, id.Pretty(), opts.DefaultResolveOpts())
+	if err == nil {
+		t.Fatal("ValidateIpnsRecord should have returned error")
+	}
+
+	// Create IPNS record path with a different private key
+	priv2, id2, _, ipnsDHTPath2 := genKeys(t)
+
+	// Make peer's public key available in peer store
+	err = peerstore.AddPubKey(id2, priv2.GetPublic())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Publish entry
+	err = PublishEntry(ctx, nvVstore, ipnsDHTPath2, entry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Record should fail validation because public key defined by
+	// ipns path doesn't match record signature
+	_, err = resolve(ctx, resolver, id2.Pretty(), opts.DefaultResolveOpts())
+	if err == nil {
+		t.Fatal("ValidateIpnsRecord should have failed signature verification")
+	}
+
+	// Publish entry without making public key available in peer store
+	priv3, id3, pubkDHTPath3, ipnsDHTPath3 := genKeys(t)
+	entry3, err := ipns.Create(priv3, p, 1, ts.Add(time.Hour))
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = PublishEntry(ctx, nvVstore, ipnsDHTPath3, entry3)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Record should fail validation because public key is not available
+	// in peer store or on network
+	_, err = resolve(ctx, resolver, id3.Pretty(), opts.DefaultResolveOpts())
+	if err == nil {
+		t.Fatal("ValidateIpnsRecord should have failed because public key was not found")
+	}
+
+	// Publish public key to the network
+	err = PublishPublicKey(ctx, vstore, pubkDHTPath3, priv3.GetPublic())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Record should now pass validation because resolver will ensure
+	// public key is available in the peer store by looking it up in
+	// the DHT, which causes the DHT to fetch it and cache it in the
+	// peer store
+	_, err = resolve(ctx, resolver, id3.Pretty(), opts.DefaultResolveOpts())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func genKeys(t *testing.T) (ci.PrivKey, peer.ID, string, string) {
+	sr := u.NewTimeSeededRand()
+	priv, _, err := ci.GenerateKeyPairWithReader(ci.RSA, 1024, sr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create entry with expiry in one hour
+	pid, err := peer.IDFromPrivateKey(priv)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return priv, pid, PkKeyForID(pid), ipns.RecordKey(pid)
+}
+
+type mockValueStore struct {
+	r     routing.ValueStore
+	kbook pstore.KeyBook
+}
+
+func newMockValueStore(id testutil.Identity, dstore ds.Datastore, kbook pstore.KeyBook) *mockValueStore {
+	return &mockValueStore{
+		r: offline.NewOfflineRouter(dstore, record.NamespacedValidator{
+			"ipns": ipns.Validator{KeyBook: kbook},
+			"pk":   record.PublicKeyValidator{},
+		}),
+		kbook: kbook,
+	}
+}
+
+func (m *mockValueStore) GetValue(ctx context.Context, k string, opts ...ropts.Option) ([]byte, error) {
+	return m.r.GetValue(ctx, k, opts...)
+}
+
+func (m *mockValueStore) SearchValue(ctx context.Context, k string, opts ...ropts.Option) (<-chan []byte, error) {
+	return m.r.SearchValue(ctx, k, opts...)
+}
+
+func (m *mockValueStore) GetPublicKey(ctx context.Context, p peer.ID) (ci.PubKey, error) {
+	pk := m.kbook.PubKey(p)
+	if pk != nil {
+		return pk, nil
+	}
+
+	pkkey := routing.KeyForPublicKey(p)
+	val, err := m.GetValue(ctx, pkkey)
+	if err != nil {
+		return nil, err
+	}
+
+	pk, err = ci.UnmarshalPublicKey(val)
+	if err != nil {
+		return nil, err
+	}
+
+	return pk, m.kbook.AddPubKey(p, pk)
+}
+
+func (m *mockValueStore) PutValue(ctx context.Context, k string, d []byte, opts ...ropts.Option) error {
+	return m.r.PutValue(ctx, k, d, opts...)
+}
