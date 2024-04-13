f122a14eaf95f93b09e30df1566562f1006f22c0
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:34:07 +0200

    Initialize go-contributing-docs

---
+++ core/coreapi/path.go
@@ -0,0 +1,75 @@
+package coreapi
+
+import (
+	"context"
+	"fmt"
+	gopath "path"
+
+	"github.com/ipfs/go-ipfs/core"
+	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
+	uio "gx/ipfs/QmWE6Ftsk98cG2MTVgH4wJT8VP2nL9TuBkYTrz9GSqcsh5/go-unixfs/io"
+	ipfspath "gx/ipfs/QmdrpbDgeYH3VxkCciQCJY5LkDYdXtig6unDzQmMxFtWEw/go-path"
+	"gx/ipfs/QmdrpbDgeYH3VxkCciQCJY5LkDYdXtig6unDzQmMxFtWEw/go-path/resolver"
+
+	"gx/ipfs/QmPSQnBKM9g7BaUcZCvswUJVscQ1ipjmwxN5PXCjkp9EQ7/go-cid"
+	ipld "gx/ipfs/QmdDXJs4axxefSPgK6Y1QhpJWKuDPnGJiqgq4uncb4rFHL/go-ipld-format"
+)
+
+// ResolveNode resolves the path `p` using Unixfs resolver, gets and returns the
+// resolved Node.
+func (api *CoreAPI) ResolveNode(ctx context.Context, p coreiface.Path) (ipld.Node, error) {
+	rp, err := api.ResolvePath(ctx, p)
+	if err != nil {
+		return nil, err
+	}
+
+	node, err := api.node.DAG.Get(ctx, rp.Cid())
+	if err != nil {
+		return nil, err
+	}
+	return node, nil
+}
+
+// ResolvePath resolves the path `p` using Unixfs resolver, returns the
+// resolved path.
+func (api *CoreAPI) ResolvePath(ctx context.Context, p coreiface.Path) (coreiface.ResolvedPath, error) {
+	if _, ok := p.(coreiface.ResolvedPath); ok {
+		return p.(coreiface.ResolvedPath), nil
+	}
+
+	ipath := ipfspath.Path(p.String())
+	ipath, err := core.ResolveIPNS(ctx, api.node.Namesys, ipath)
+	if err == core.ErrNoNamesys {
+		return nil, coreiface.ErrOffline
+	} else if err != nil {
+		return nil, err
+	}
+
+	var resolveOnce resolver.ResolveOnce
+
+	switch ipath.Segments()[0] {
+	case "ipfs":
+		resolveOnce = uio.ResolveUnixfsOnce
+	case "ipld":
+		resolveOnce = resolver.ResolveSingle
+	default:
+		return nil, fmt.Errorf("unsupported path namespace: %s", p.Namespace())
+	}
+
+	r := &resolver.Resolver{
+		DAG:         api.node.DAG,
+		ResolveOnce: resolveOnce,
+	}
+
+	node, rest, err := r.ResolveToLastNode(ctx, ipath)
+	if err != nil {
+		return nil, err
+	}
+
+	root, err := cid.Parse(ipath.Segments()[1])
+	if err != nil {
+		return nil, err
+	}
+
+	return coreiface.NewResolvedPath(ipath, node, root, gopath.Join(rest...)), nil
+}
---
+++ p2p/p2p.go
@@ -0,0 +1,53 @@
+package p2p
+
+import (
+	pstore "gx/ipfs/QmWtCpWB39Rzc2xTB75MKorsxNpo3TyecTEN24CJ3KVohE/go-libp2p-peerstore"
+	logging "gx/ipfs/QmZChCsSt8DctjceaL56Eibc29CVQq4dGKRXC5JRZ6Ppae/go-log"
+	peer "gx/ipfs/QmbNepETomvmXfz1X5pHNFD2QuPqnqi47dTd94QJWSorQ3/go-libp2p-peer"
+	p2phost "gx/ipfs/Qmf5yHzmWAyHSJRPAmZzfk3Yd7icydBLi7eec5741aov7v/go-libp2p-host"
+)
+
+var log = logging.Logger("p2p-mount")
+
+// P2P structure holds information on currently running streams/Listeners
+type P2P struct {
+	ListenersLocal *Listeners
+	ListenersP2P   *Listeners
+	Streams        *StreamRegistry
+
+	identity  peer.ID
+	peerHost  p2phost.Host
+	peerstore pstore.Peerstore
+}
+
+// NewP2P creates new P2P struct
+func NewP2P(identity peer.ID, peerHost p2phost.Host, peerstore pstore.Peerstore) *P2P {
+	return &P2P{
+		identity:  identity,
+		peerHost:  peerHost,
+		peerstore: peerstore,
+
+		ListenersLocal: newListenersLocal(),
+		ListenersP2P:   newListenersP2P(peerHost),
+
+		Streams: &StreamRegistry{
+			Streams:     map[uint64]*Stream{},
+			ConnManager: peerHost.ConnManager(),
+			conns:       map[peer.ID]int{},
+		},
+	}
+}
+
+// CheckProtoExists checks whether a proto handler is registered to
+// mux handler
+func (p2p *P2P) CheckProtoExists(proto string) bool {
+	protos := p2p.peerHost.Mux().Protocols()
+
+	for _, p := range protos {
+		if p != proto {
+			continue
+		}
+		return true
+	}
+	return false
+}
