b08071c867073fbbc4b25e6dee33856f775105f8
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:36:37 +0200

    Initialize feat/better-errors

---
+++ bin/maketarball.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# vim: set expandtab sw=2 ts=2:
+
+# bash safe mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# readlink doesn't work on macos
+OUTPUT="${1:-go-ipfs-source.tar.gz}"
+if ! [[ "$OUTPUT" = /* ]]; then
+  OUTPUT="$PWD/$OUTPUT"
+fi
+
+GOCC=${GOCC=go}
+
+TMPDIR="$(mktemp -d)"
+cp -r . "$TMPDIR"
+( cd "$TMPDIR" &&
+  echo $PWD &&
+  $GOCC mod vendor &&
+  (git describe --always --match=NeVeRmAtCh --dirty 2>/dev/null || true) > .tarball &&
+  chmod -R u=rwX,go=rX "$TMPDIR" # normalize permissions
+  tar -czf "$OUTPUT" --exclude="./.git" .
+  )
+
+rm -rf "$TMPDIR"
