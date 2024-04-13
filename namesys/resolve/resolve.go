c4840a5654efd1bc3d8788ba106957b4643a9fea
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:38:12 +0200

    Initialize fix/dht-put-docs

---
+++ namesys/resolve/resolve.go
@@ -0,0 +1,67 @@
+package resolve
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/go-path"
+	"github.com/ipfs/go-path/resolver"
+
+	"github.com/ipfs/go-ipfs/namesys"
+)
+
+// ErrNoNamesys is an explicit error for when an IPFS node doesn't
+// (yet) have a name system
+var ErrNoNamesys = errors.New(
+	"core/resolve: no Namesys on IpfsNode - can't resolve ipns entry")
+
+// ResolveIPNS resolves /ipns paths
+func ResolveIPNS(ctx context.Context, nsys namesys.NameSystem, p path.Path) (path.Path, error) {
+	if strings.HasPrefix(p.String(), "/ipns/") {
+		// TODO(cryptix): we should be able to query the local cache for the path
+		if nsys == nil {
+			return "", ErrNoNamesys
+		}
+
+		seg := p.Segments()
+
+		if len(seg) < 2 || seg[1] == "" { // just "/<protocol/>" without further segments
+			err := fmt.Errorf("invalid path %q: ipns path missing IPNS ID", p)
+			return "", err
+		}
+
+		extensions := seg[2:]
+		resolvable, err := path.FromSegments("/", seg[0], seg[1])
+		if err != nil {
+			return "", err
+		}
+
+		respath, err := nsys.Resolve(ctx, resolvable.String())
+		if err != nil {
+			return "", err
+		}
+
+		segments := append(respath.Segments(), extensions...)
+		p, err = path.FromSegments("/", segments...)
+		if err != nil {
+			return "", err
+		}
+	}
+	return p, nil
+}
+
+// Resolve resolves the given path by parsing out protocol-specific
+// entries (e.g. /ipns/<node-key>) and then going through the /ipfs/
+// entries and returning the final node.
+func Resolve(ctx context.Context, nsys namesys.NameSystem, r *resolver.Resolver, p path.Path) (format.Node, error) {
+	p, err := ResolveIPNS(ctx, nsys, p)
+	if err != nil {
+		return nil, err
+	}
+
+	// ok, we have an IPFS path now (or what we'll treat as one)
+	return r.ResolvePath(ctx, p)
+}
