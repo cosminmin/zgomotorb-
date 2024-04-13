438eeaed7507a153adc38da7680bd82c87ba40bd
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:34:34 +0200

    Initialize kevina/multibase3

---
+++ core/coreunix/metadata_test.go
@@ -0,0 +1,94 @@
+package coreunix
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"testing"
+
+	core "github.com/ipfs/go-ipfs/core"
+	ft "gx/ipfs/QmUnHNqhSB1JgzVCxL1Kz3yb4bdyB4q1Z9AD5AUBVmt3fZ/go-unixfs"
+	importer "gx/ipfs/QmUnHNqhSB1JgzVCxL1Kz3yb4bdyB4q1Z9AD5AUBVmt3fZ/go-unixfs/importer"
+	uio "gx/ipfs/QmUnHNqhSB1JgzVCxL1Kz3yb4bdyB4q1Z9AD5AUBVmt3fZ/go-unixfs/io"
+	bserv "gx/ipfs/QmVDTbzzTwnuBwNbJdhW3u7LoBQp46bezm9yp4z1RoEepM/go-blockservice"
+	merkledag "gx/ipfs/QmcGt25mrjuB2kKW2zhPbXVZNHc4yoTDQ65NA8m6auP2f1/go-merkledag"
+
+	u "gx/ipfs/QmNohiVssaPw3KVLZik59DBVGTSm2dGvYT9eoXt5DQ36Yz/go-ipfs-util"
+	chunker "gx/ipfs/QmR4QQVkBZsZENRjYFVi8dEtPL3daZRNKk24m4r6WKJHNm/go-ipfs-chunker"
+	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
+	bstore "gx/ipfs/QmS2aqUZLJp8kF1ihE5rvDGE5LvmKDPnx32w9Z1BW9xLV5/go-ipfs-blockstore"
+	offline "gx/ipfs/QmYZwey1thDTynSrvd6qQkX24UpTka6TFhQ2v569UpoqxD/go-ipfs-exchange-offline"
+	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
+	ds "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore"
+	dssync "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore/sync"
+)
+
+func getDagserv(t *testing.T) ipld.DAGService {
+	db := dssync.MutexWrap(ds.NewMapDatastore())
+	bs := bstore.NewBlockstore(db)
+	blockserv := bserv.New(bs, offline.Exchange(bs))
+	return merkledag.NewDAGService(blockserv)
+}
+
+func TestMetadata(t *testing.T) {
+	ctx := context.Background()
+	// Make some random node
+	ds := getDagserv(t)
+	data := make([]byte, 1000)
+	u.NewTimeSeededRand().Read(data)
+	r := bytes.NewReader(data)
+	nd, err := importer.BuildDagFromReader(ds, chunker.DefaultSplitter(r))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c := nd.Cid()
+
+	m := new(ft.Metadata)
+	m.MimeType = "THIS IS A TEST"
+
+	// Such effort, many compromise
+	ipfsnode := &core.IpfsNode{DAG: ds}
+
+	mdk, err := AddMetadataTo(ipfsnode, c.String(), m)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec, err := Metadata(ipfsnode, mdk)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rec.MimeType != m.MimeType {
+		t.Fatalf("something went wrong in conversion: '%s' != '%s'", rec.MimeType, m.MimeType)
+	}
+
+	cdk, err := cid.Decode(mdk)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	retnode, err := ds.Get(ctx, cdk)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rtnpb, ok := retnode.(*merkledag.ProtoNode)
+	if !ok {
+		t.Fatal("expected protobuf node")
+	}
+
+	ndr, err := uio.NewDagReader(ctx, rtnpb, ds)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := ioutil.ReadAll(ndr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(out, data) {
+		t.Fatal("read incorrect data")
+	}
+}
---
+++ namesys/namesys_test.go
@@ -0,0 +1,109 @@
+package namesys
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	opts "github.com/ipfs/go-ipfs/namesys/opts"
+
+	ci "gx/ipfs/QmNiJiXwWE3kRhZrC5ej3kSjWHm337pYfhjLGSCDNKJP2s/go-libp2p-crypto"
+	pstoremem "gx/ipfs/QmQAGG1zxfePqj2t7bLxyN8AFccZ889DDR9Gn8kVLDrGZo/go-libp2p-peerstore/pstoremem"
+	ipns "gx/ipfs/QmR9UpasSQR4Mqq1qiJAfnY4SVBxJn7r639CxiLjx8dYGm/go-ipns"
+	"gx/ipfs/QmUnHNqhSB1JgzVCxL1Kz3yb4bdyB4q1Z9AD5AUBVmt3fZ/go-unixfs"
+	path "gx/ipfs/QmVi2uUygezqaMTqs3Yzt5FcZFHJoYD4B7jQ2BELjj7ZuY/go-path"
+	peer "gx/ipfs/QmcqU6QUDSXprb1518vYDGczrTJTyGwLG9eUa5iNX4xUtS/go-libp2p-peer"
+	offroute "gx/ipfs/QmdxhyAwBrnmJFsYPK6tyHh4Yy3gK8gbULErX1dRnpUMqu/go-ipfs-routing/offline"
+	ds "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore"
+	dssync "gx/ipfs/Qmf4xQhNomPNhrtZc67qSnfJSjxjXs9LWvknJtSXwimPrM/go-datastore/sync"
+)
+
+type mockResolver struct {
+	entries map[string]string
+}
+
+func testResolution(t *testing.T, resolver Resolver, name string, depth uint, expected string, expError error) {
+	t.Helper()
+	p, err := resolver.Resolve(context.Background(), name, opts.Depth(depth))
+	if err != expError {
+		t.Fatal(fmt.Errorf(
+			"expected %s with a depth of %d to have a '%s' error, but got '%s'",
+			name, depth, expError, err))
+	}
+	if p.String() != expected {
+		t.Fatal(fmt.Errorf(
+			"%s with depth %d resolved to %s != %s",
+			name, depth, p.String(), expected))
+	}
+}
+
+func (r *mockResolver) resolveOnceAsync(ctx context.Context, name string, options opts.ResolveOpts) <-chan onceResult {
+	p, err := path.ParsePath(r.entries[name])
+	out := make(chan onceResult, 1)
+	out <- onceResult{value: p, err: err}
+	close(out)
+	return out
+}
+
+func mockResolverOne() *mockResolver {
+	return &mockResolver{
+		entries: map[string]string{
+			"QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy": "/ipfs/Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj",
+			"QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n": "/ipns/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy",
+			"QmY3hE8xgFCjGcz6PHgnvJz5HZi1BaKRfPkn1ghZUcYMjD": "/ipns/ipfs.io",
+		},
+	}
+}
+
+func mockResolverTwo() *mockResolver {
+	return &mockResolver{
+		entries: map[string]string{
+			"ipfs.io": "/ipns/QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n",
+		},
+	}
+}
+
+func TestNamesysResolution(t *testing.T) {
+	r := &mpns{
+		ipnsResolver: mockResolverOne(),
+		dnsResolver:  mockResolverTwo(),
+	}
+
+	testResolution(t, r, "Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj", opts.DefaultDepthLimit, "/ipfs/Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj", nil)
+	testResolution(t, r, "/ipns/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy", opts.DefaultDepthLimit, "/ipfs/Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj", nil)
+	testResolution(t, r, "/ipns/QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n", opts.DefaultDepthLimit, "/ipfs/Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj", nil)
+	testResolution(t, r, "/ipns/QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n", 1, "/ipns/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy", ErrResolveRecursion)
+	testResolution(t, r, "/ipns/ipfs.io", opts.DefaultDepthLimit, "/ipfs/Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj", nil)
+	testResolution(t, r, "/ipns/ipfs.io", 1, "/ipns/QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n", ErrResolveRecursion)
+	testResolution(t, r, "/ipns/ipfs.io", 2, "/ipns/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy", ErrResolveRecursion)
+	testResolution(t, r, "/ipns/QmY3hE8xgFCjGcz6PHgnvJz5HZi1BaKRfPkn1ghZUcYMjD", opts.DefaultDepthLimit, "/ipfs/Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj", nil)
+	testResolution(t, r, "/ipns/QmY3hE8xgFCjGcz6PHgnvJz5HZi1BaKRfPkn1ghZUcYMjD", 1, "/ipns/ipfs.io", ErrResolveRecursion)
+	testResolution(t, r, "/ipns/QmY3hE8xgFCjGcz6PHgnvJz5HZi1BaKRfPkn1ghZUcYMjD", 2, "/ipns/QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n", ErrResolveRecursion)
+	testResolution(t, r, "/ipns/QmY3hE8xgFCjGcz6PHgnvJz5HZi1BaKRfPkn1ghZUcYMjD", 3, "/ipns/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy", ErrResolveRecursion)
+}
+
+func TestPublishWithCache0(t *testing.T) {
+	dst := dssync.MutexWrap(ds.NewMapDatastore())
+	priv, _, err := ci.GenerateKeyPair(ci.RSA, 1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ps := pstoremem.NewPeerstore()
+	pid, err := peer.IDFromPrivateKey(priv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ps.AddPrivKey(pid, priv)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	routing := offroute.NewOfflineRouter(dst, ipns.Validator{KeyBook: ps})
+
+	nsys := NewNameSystem(routing, dst, 0)
+	p, err := path.ParsePath(unixfs.EmptyDirNode().Cid().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	nsys.Publish(context.Background(), priv, p)
+}
