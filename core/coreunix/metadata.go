feee4c5306354a877a2fc481a7a2fe0423b4c7ed
cosmida <79363405+cosminmin@users.noreply.github.com>
2024-04-13 20:35:11 +0200

    Initialize tests/coreapi-seek

---
+++ core/coreunix/metadata.go
@@ -0,0 +1,57 @@
+package coreunix
+
+import (
+	core "github.com/ipfs/go-ipfs/core"
+	ft "gx/ipfs/QmQ1JnYpnzkaurjW1yxkQxC2w3K1PorNE1nv1vaP5Le7sq/go-unixfs"
+	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
+	dag "gx/ipfs/Qmb2UEG2TAeVrEJSjqsZF7Y2he7wRDkrdt6c3bECxwZf4k/go-merkledag"
+)
+
+func AddMetadataTo(n *core.IpfsNode, skey string, m *ft.Metadata) (string, error) {
+	c, err := cid.Decode(skey)
+	if err != nil {
+		return "", err
+	}
+
+	nd, err := n.DAG.Get(n.Context(), c)
+	if err != nil {
+		return "", err
+	}
+
+	mdnode := new(dag.ProtoNode)
+	mdata, err := ft.BytesForMetadata(m)
+	if err != nil {
+		return "", err
+	}
+
+	mdnode.SetData(mdata)
+	if err := mdnode.AddNodeLink("file", nd); err != nil {
+		return "", err
+	}
+
+	err = n.DAG.Add(n.Context(), mdnode)
+	if err != nil {
+		return "", err
+	}
+
+	return mdnode.Cid().String(), nil
+}
+
+func Metadata(n *core.IpfsNode, skey string) (*ft.Metadata, error) {
+	c, err := cid.Decode(skey)
+	if err != nil {
+		return nil, err
+	}
+
+	nd, err := n.DAG.Get(n.Context(), c)
+	if err != nil {
+		return nil, err
+	}
+
+	pbnd, ok := nd.(*dag.ProtoNode)
+	if !ok {
+		return nil, dag.ErrNotProtobuf
+	}
+
+	return ft.MetadataFromBytes(pbnd.Data())
+}
---
+++ test/integration/three_legged_cat_test.go
@@ -0,0 +1,144 @@
+package integrationtest
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"math"
+	"testing"
+	"time"
+
+	core "github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/coreapi"
+	mock "github.com/ipfs/go-ipfs/core/mock"
+	"github.com/ipfs/go-ipfs/thirdparty/unit"
+
+	testutil "gx/ipfs/QmNvHv84aH2qZafDuSdKJCQ1cvPZ1kmQmyD4YtzjUHuk9v/go-testutil"
+	pstore "gx/ipfs/QmPiemjiKBC9VA7vZF82m4x1oygtg2c2YVqag8PX7dN1BD/go-libp2p-peerstore"
+	mocknet "gx/ipfs/QmYxivS34F2M2n44WQQnRHGAKS8aoRUxwGpi9wk4Cdn4Jf/go-libp2p/p2p/net/mock"
+	files "gx/ipfs/QmaXvvAVAQ5ABqM5xtjYmV85xmN5MkWAZsX9H9Fwo4FVXp/go-ipfs-files"
+)
+
+func TestThreeLeggedCatTransfer(t *testing.T) {
+	conf := testutil.LatencyConfig{
+		NetworkLatency:    0,
+		RoutingLatency:    0,
+		BlockstoreLatency: 0,
+	}
+	if err := RunThreeLeggedCat(RandomBytes(100*unit.MB), conf); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestThreeLeggedCatDegenerateSlowBlockstore(t *testing.T) {
+	SkipUnlessEpic(t)
+	conf := testutil.LatencyConfig{BlockstoreLatency: 50 * time.Millisecond}
+	if err := RunThreeLeggedCat(RandomBytes(1*unit.KB), conf); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestThreeLeggedCatDegenerateSlowNetwork(t *testing.T) {
+	SkipUnlessEpic(t)
+	conf := testutil.LatencyConfig{NetworkLatency: 400 * time.Millisecond}
+	if err := RunThreeLeggedCat(RandomBytes(1*unit.KB), conf); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestThreeLeggedCatDegenerateSlowRouting(t *testing.T) {
+	SkipUnlessEpic(t)
+	conf := testutil.LatencyConfig{RoutingLatency: 400 * time.Millisecond}
+	if err := RunThreeLeggedCat(RandomBytes(1*unit.KB), conf); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestThreeLeggedCat100MBMacbookCoastToCoast(t *testing.T) {
+	SkipUnlessEpic(t)
+	conf := testutil.LatencyConfig{}.NetworkNYtoSF().BlockstoreSlowSSD2014().RoutingSlow()
+	if err := RunThreeLeggedCat(RandomBytes(100*unit.MB), conf); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func RunThreeLeggedCat(data []byte, conf testutil.LatencyConfig) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// create network
+	mn := mocknet.New(ctx)
+	mn.SetLinkDefaults(mocknet.LinkOptions{
+		Latency: conf.NetworkLatency,
+		// TODO add to conf. This is tricky because we want 0 values to be functional.
+		Bandwidth: math.MaxInt32,
+	})
+
+	bootstrap, err := core.NewNode(ctx, &core.BuildCfg{
+		Online: true,
+		Host:   mock.MockHostOption(mn),
+	})
+	if err != nil {
+		return err
+	}
+	defer bootstrap.Close()
+
+	adder, err := core.NewNode(ctx, &core.BuildCfg{
+		Online: true,
+		Host:   mock.MockHostOption(mn),
+	})
+	if err != nil {
+		return err
+	}
+	defer adder.Close()
+
+	catter, err := core.NewNode(ctx, &core.BuildCfg{
+		Online: true,
+		Host:   mock.MockHostOption(mn),
+	})
+	if err != nil {
+		return err
+	}
+	defer catter.Close()
+
+	adderApi, err := coreapi.NewCoreAPI(adder)
+	if err != nil {
+		return err
+	}
+
+	catterApi, err := coreapi.NewCoreAPI(catter)
+	if err != nil {
+		return err
+	}
+
+	mn.LinkAll()
+
+	bis := bootstrap.Peerstore.PeerInfo(bootstrap.PeerHost.ID())
+	bcfg := core.BootstrapConfigWithPeers([]pstore.PeerInfo{bis})
+	if err := adder.Bootstrap(bcfg); err != nil {
+		return err
+	}
+	if err := catter.Bootstrap(bcfg); err != nil {
+		return err
+	}
+
+	added, err := adderApi.Unixfs().Add(ctx, files.NewBytesFile(data))
+	if err != nil {
+		return err
+	}
+
+	readerCatted, err := catterApi.Unixfs().Get(ctx, added)
+	if err != nil {
+		return err
+	}
+
+	// verify
+	bufout := new(bytes.Buffer)
+	io.Copy(bufout, readerCatted.(io.Reader))
+	if 0 != bytes.Compare(bufout.Bytes(), data) {
+		return errors.New("catted data does not match added data")
+	}
+	cancel()
+	return nil
+}
